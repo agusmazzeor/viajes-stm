6345febd2ccb6bc145c31ba652c8c9f20737cd81
Natasha Tenreiro <natasha.tenreiro@cedarcode.com>
2024-07-18 20:43:20 -0300

    Cambiamos StopData por DataViaje

--- main.cpp
+++ main.cpp
@@ -26,6 +26,17 @@ using TipoDiaMap = map<string, RecorridoMap>;  // clave: id_tipo_dia
 using LineaMap = map<string, TipoDiaMap>;      // clave: linea
 // lista_horarios_teoricos_parada[linea][id_tipo_dia][id_recorrido][id_parada] = [delay,cantidad_boletos_vendidos,pos_recorrido,horario,arranco_dia_aterior]
 
+struct DataViaje
+{
+    string fecha_evento; // horario
+    string cantidad_pasajeros;
+    string codigo_parada_origen; // codigo de la parada de ascenso
+    string cod_empresa;          // codigo empresa a la cual pertenece el omnibus
+    string linea_codigo;         // codigo de la linea
+    string dsc_linea;            // nombre publico de la linea
+    string sevar_codigo;         // codigo de la variante
+};
+
 // Función para dividir una cadena en partes, dado un delimitador
 vector<string> split(const string &s, char delimiter)
 {
@@ -95,7 +106,7 @@ struct StopData
     double avg_speed;
 };
 
-void load_chunk_data(const string &filename, vector<StopData> &data, int start, int count)
+void load_chunk_data(const string &filename, vector<DataViaje> &data, int start, int count, const LineaMap &horarios_teoricos)
 {
     ifstream file(filename);
     string line;
@@ -114,29 +125,32 @@ void load_chunk_data(const string &filename, vector<StopData> &data, int start, 
         {
             if (getline(file, line))
             {
-                stringstream ss(line);
-                StopData stop_data;
-                string temp;
+                vector<string> tokens = split(line, ';');
+                DataViaje viaje;
 
-                // Leer los campos de cada línea
-                getline(ss, stop_data.stop_id, ',');
-                getline(ss, temp, ',');
-                stop_data.theoretical_time = stod(temp);
-                getline(ss, temp, ',');
-                stop_data.actual_time = stod(temp);
-                getline(ss, temp, ',');
-                stop_data.avg_speed = stod(temp);
+                viaje.fecha_evento = tokens[2];
+                viaje.cantidad_pasajeros = tokens[10];
+                viaje.codigo_parada_origen = tokens[11];
+                viaje.cod_empresa = tokens[12];
+                viaje.linea_codigo = tokens[14];
+                viaje.dsc_linea = tokens[15];
+                viaje.sevar_codigo = tokens[16];
 
-                // Agregar los datos al vector
-                data.push_back(stop_data);
+                // Agregar solo si sevar_codigo está en lineas_a_evaluar
+                if (horarios_teoricos.find(viaje.sevar_codigo) != horarios_teoricos.end())
+                {
+                    data.push_back(viaje);
+                }
             }
         }
     }
 }
 
-double calculate_delay(const StopData &data)
+// TODO
+double calculate_delay(const DataViaje &data, const LineaMap &horarios_teoricos)
 {
-    return data.actual_time - data.theoretical_time;
+    // Aquí debes implementar la lógica para calcular el retraso basado en los horarios teóricos
+    return 0.0; // Retorna 0 si no se encuentra información correspondiente
 }
 
 void serialize_map(const unordered_map<string, double> &map, vector<char> &buffer)
@@ -165,7 +179,49 @@ void deserialize_map(const vector<char> &buffer, unordered_map<string, double> &
     }
 }
 
-void helper_function(LineaMap &lista_horarios_teoricos_parada)
+void serialize_horarios_teoricos(const LineaMap &schedule, string &output)
+{
+    stringstream ss;
+    for (const auto &linea : schedule)
+    {
+        for (const auto &id_tipo_dia : linea.second)
+        {
+            for (const auto &id_recorrido : id_tipo_dia.second)
+            {
+                for (const auto &id_parada : id_recorrido.second)
+                {
+                    const HorarioTeorico &ht = id_parada.second;
+                    ss << linea.first << "," << id_tipo_dia.first << "," << id_recorrido.first << "," << id_parada.first << ","
+                       << ht.delay << "," << ht.cantidad_boletos_vendidos << "," << ht.pos_recorrido << ","
+                       << ht.horario << "," << ht.arranco_dia_anterior << "\n";
+                }
+            }
+        }
+    }
+    output = ss.str();
+}
+
+void deserialize_horarios_teoricos(const string &input, LineaMap &schedule)
+{
+    stringstream ss(input);
+    string line;
+    while (getline(ss, line))
+    {
+        vector<string> tokens = split(line, ',');
+        if (tokens.size() == 9)
+        {
+            HorarioTeorico ht;
+            ht.delay = stoi(tokens[4]);
+            ht.cantidad_boletos_vendidos = stoi(tokens[5]);
+            ht.pos_recorrido = stoi(tokens[6]);
+            ht.horario = tokens[7];
+            ht.arranco_dia_anterior = tokens[8] == "1";
+            schedule[tokens[0]][tokens[1]][tokens[2]][tokens[3]] = ht;
+        }
+    }
+}
+
+void helper_function_print_linea_map(LineaMap &lista_horarios_teoricos_parada)
 {
     // Imprimir algunos datos para verificar la estructura
     for (const auto &linea : lista_horarios_teoricos_parada)
@@ -192,6 +248,21 @@ void helper_function(LineaMap &lista_horarios_teoricos_parada)
     }
 }
 
+void print_data_viaje(const vector<DataViaje> &data_viajes)
+{
+    for (const auto &viaje : data_viajes)
+    {
+        cout << "Fecha del evento: " << viaje.fecha_evento << endl;
+        cout << "Cantidad de pasajeros: " << viaje.cantidad_pasajeros << endl;
+        cout << "Código de parada origen: " << viaje.codigo_parada_origen << endl;
+        cout << "Código de la empresa: " << viaje.cod_empresa << endl;
+        cout << "Código de la línea: " << viaje.linea_codigo << endl;
+        cout << "Descripción de la línea: " << viaje.dsc_linea << endl;
+        cout << "Código de variante (sevar_codigo): " << viaje.sevar_codigo << endl;
+        cout << "------------------------" << endl;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     MPI_Init(&argc, &argv);
@@ -200,13 +271,29 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    const string filename = "stop_data.csv";
+    const string filename = "05_2024_viajes.csv";
 
     if (rank == 0)
     {
         // Master process
-
         LineaMap lista_horarios_teoricos_parada = procesar_horarios_teoricos();
+
+        // Serializar el mapa
+        string serialized_schedule;
+        serialize_horarios_teoricos(lista_horarios_teoricos_parada, serialized_schedule);
+
+        // Enviar el tamaño del mapa serializado
+        int schedule_size = serialized_schedule.size();
+        for (int i = 1; i < size; ++i)
+        {
+            MPI_Send(&schedule_size, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+        }
+
+        // Enviar el mapa serializado
+        for (int i = 1; i < size; ++i)
+        {
+            MPI_Send(serialized_schedule.data(), schedule_size, MPI_CHAR, i, 0, MPI_COMM_WORLD);
+        }
 
         ifstream file(filename);
         string line;
@@ -263,26 +350,41 @@ int main(int argc, char *argv[])
     else
     {
         // Proceso esclavo
+        // Recibir el tamaño del mapa serializado
+        int schedule_size;
+        MPI_Recv(&schedule_size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        // Recibir el mapa serializado
+        vector<char> buffer(schedule_size);
+        MPI_Recv(buffer.data(), schedule_size, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        // Deserializar el mapa
+        LineaMap lista_horarios_teoricos_parada;
+        string serialized_schedule(buffer.begin(), buffer.end());
+        deserialize_horarios_teoricos(serialized_schedule, lista_horarios_teoricos_parada);
+
         int start, count;
         MPI_Recv(&start, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         MPI_Recv(&count, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-        vector<StopData> slave_data;
-        load_chunk_data(filename, slave_data, start, count);
+        vector<DataViaje> slave_data;
+        load_chunk_data(filename, slave_data, start, count, lista_horarios_teoricos_parada);
+
+        print_data_viaje(slave_data);
 
         unordered_map<string, double> slave_delay_map;
-        for (const auto &stop : slave_data)
+        for (const auto &viaje : slave_data)
         {
-            double delay = calculate_delay(stop);
-            slave_delay_map[stop.stop_id] += delay;
+            double delay = calculate_delay(viaje, lista_horarios_teoricos_parada);
+            slave_delay_map[viaje.sevar_codigo] += delay;
         }
 
-        vector<char> buffer;
-        serialize_map(slave_delay_map, buffer);
+        vector<char> buffer_out;
+        serialize_map(slave_delay_map, buffer_out);
 
-        int map_size = buffer.size();
+        int map_size = buffer_out.size();
         MPI_Send(&map_size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-        MPI_Send(buffer.data(), map_size, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(buffer_out.data(), map_size, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
 
         cout << "Termine slave con rank: " << rank << ", con slave_delay_map de size: " << slave_delay_map.size() << endl;
     }
