b61029b982c8b803ab62308951afc45ddd38a441
Natasha Tenreiro <natasha.tenreiro@cedarcode.com>
2024-07-26 20:08:50 -0300

    Elimino toda la data que no es necesaria para hacer espacio

--- types.h
+++ types.h
@@ -9,10 +9,9 @@ using namespace std;
 // const string DATOS_VIAJES = "datos_dummy/05_2024_viajes_reducido.csv";
 // const string HORARIOS_POR_PARADA = "datos_dummy/horarios_por_parada.csv";
 // const string PARADAS = "datos_dummy/paradas.csv";
-const string DATOS_VIAJES = "datos/viajes/output_2.csv";
-// const string DATOS_VIAJES = "datos/viajes/viajes_stm_052024_reducido.csv";
-const string HORARIOS_POR_PARADA = "datos/horarios_teoricos/horarios_por_parada.csv";
-const string PARADAS = "datos/paradas/paradas.csv";
+const string DATOS_VIAJES = "datos/viajes_stm.csv";
+const string HORARIOS_POR_PARADA = "datos/horarios_por_parada.csv";
+const string PARADAS = "datos/paradas.csv";
 
 // Definición de la estructura para la información de los horarios teóricos de una parada
 struct HorarioTeorico
