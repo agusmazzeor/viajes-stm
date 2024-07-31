0fed573d3088f4922c21cf64fc5ec245a6abd5cd
Natasha Tenreiro <natasha.tenreiro@cedarcode.com>
2024-07-31 09:11:40 -0300

    Cambio datos del script para correr en el cluster

--- ejecucion_cluster/ejecutar_programa.sh
+++ ejecucion_cluster/ejecutar_programa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=mitrabajo
-#SBATCH --ntasks=3
-#SBATCH --mem-per-cpu=16384
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=1024
 #SBATCH --time=10:00:00
 #SBATCH --partition=normal
 #SBATCH --qos=normal
@@ -18,5 +18,5 @@ do
 	echo "1 proceso con 16gb de RAM"
 	make clean
 	make
-	mpirun -np 3 ./bin/main
+	mpirun -np 2 ./bin/main
 done
