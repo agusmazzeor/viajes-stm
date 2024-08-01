f9b3244362836f204d4b124f68df21bb8ad06170
Natasha Tenreiro <natasha.tenreiro@cedarcode.com>
2024-07-31 22:48:05 -0300

    Para ejecutar en cluster

--- ejecucion_cluster/ejecutar_programa.sh
+++ ejecucion_cluster/ejecutar_programa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=mitrabajo
-#SBATCH --ntasks=3
-#SBATCH --mem-per-cpu=16384
+#SBATCH --ntasks=10
+#SBATCH --mem-per-cpu=32768
 #SBATCH --time=10:00:00
 #SBATCH --partition=normal
 #SBATCH --qos=normal
@@ -18,5 +18,5 @@ do
 	# echo "1 proceso con 16gb de RAM"
 	make clean
 	make
-	mpirun -np 3 ./bin/main
+	mpirun -np 10 ./bin/main
 done
